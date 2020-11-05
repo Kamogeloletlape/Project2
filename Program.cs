581acdd68c3e880a8c8fbcc7cd2751f848d8233e
Kamogeloletlape <60926303+Kamogeloletlape@users.noreply.github.com>
2020-11-05 09:38:05 +0200

    project startover

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace project2_2nd
+namespace dimension
 {
     public class Program
     {
--- Startup.cs
+++ Startup.cs
@@ -8,12 +8,12 @@ using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
-using project2_2nd.Data;
+using dimension.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace project2_2nd
+namespace dimension
 {
     public class Startup
     {
